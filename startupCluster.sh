83b983a5798e412e1a4c93ec0e5f2bdc4d873eed
alastairyang <dyang@wesleyan.edu>
2026-06-09 10:52:04 -0400

    update cluster startup file

--- startupCluster.sh
+++ startupCluster.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#SBATCH --job-name=data-setup
+#SBATCH --account=gts-wchu38
+#SBATCH --partition=cpu-small    # or cpu-large
+#SBATCH --qos=inferno
+#SBATCH --nodes=1
+#SBATCH --ntasks=1               # only 1 process (Python script)
+#SBATCH --cpus-per-task=4        # n cores to fork workers into
+#SBATCH --mem=4G                
+#SBATCH --time=00:10:00           
+#SBATCH --array=1          
+#SBATCH --output=/storage/project/r-wchu38-0/dyang379/logs/data-setup_%A_%a.out
+#SBATCH --error=/storage/project/r-wchu38-0/dyang379/logs/data-setup_%A_%a.err
 
 module purge
 module load uv
